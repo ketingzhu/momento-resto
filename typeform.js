1de1b5aa4cba4db30e427ee490b8e28c17e3d094
ketingzhu <81485218+ketingzhu@users.noreply.github.com>
2024-05-26 18:15:24 +0200

    Add files via upload

---
+++ typeform.js
@@ -0,0 +1,20 @@
+// Define a function to handle button click
+function openTypeform(event) {
+    event.preventDefault(); // Prevent default link behavior (navigating to href)
+    
+    // Create a new Typeform embed object and open it
+    var typeformEmbed = new TypeformEmbed({
+        url: 'https://0rx2ndldz76.typeform.com/to/rDAtbYfC',
+        hideHeaders: true,
+        hideFooter: true
+    });
+    typeformEmbed.open();
+}
+
+// Add event listener to the button
+document.addEventListener('DOMContentLoaded', function() {
+    var openTypeformButton = document.getElementById('openTypeformButton');
+    if (openTypeformButton) {
+        openTypeformButton.addEventListener('click', openTypeform);
+    }
+});
